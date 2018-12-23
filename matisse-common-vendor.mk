91f067676fdb609270611f78b2b090c35a49de2a
DJABHipHop <castellanos.kevin@rocketmail.com>
2018-12-23 15:26:07 -0500

    Adding missing libs by @matteo0026
    Adding missing libs by @matteo0026

--- matisse-common-vendor.mk
+++ matisse-common-vendor.mk
@@ -329,4 +329,16 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/matisse-common/proprietary/vendor/lib/libmm-abl-oem.so:system/vendor/lib/libmm-abl-oem.so \
     vendor/samsung/matisse-common/proprietary/vendor/lib/liboemcrypto.so:system/vendor/lib/liboemcrypto.so
 
+# Missing Lib by @matteo0026
+PRODUCT_COPY_FILES += \
+    vendor/samsung/matisse-common/proprietary/vendor/lib/libdiag.so \
+    vendor/samsung/matisse-common/proprietary/vendor/lib/libdsutils.so \
+    vendor/samsung/matisse-common/proprietary/vendor/lib/libidl.so \
+    vendor/samsung/matisse-common/proprietary/vendor/lib/libqmi_cci.so \
+    vendor/samsung/matisse-common/proprietary/vendor/lib/libqmi_client_qmux.so \
+    vendor/samsung/matisse-common/proprietary/vendor/lib/libqmi_common_so.so \
+    vendor/samsung/matisse-common/proprietary/vendor/lib/libdiag.so \
+    vendor/samsung/matisse-common/proprietary/vendor/lib/libqmi_encdec.so \
+    vendor/samsung/matisse-common/proprietary/vendor/lib/libqmiservices.so
+
 $(call inherit-product, vendor/qcom/binaries/msm8974/graphics/graphics-vendor.mk)

bd169c997aa875322ef52fda346493afc1f9439f
matteo0026 <marmotta200026@gmail.com>
2019-01-30 22:34:25 +0100

    Fix typo

--- matisse-common-vendor.mk
+++ matisse-common-vendor.mk
@@ -347,7 +347,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/matisse-common/proprietary/vendor/lib/libqmi_encdec.so:system/vendor/lib/libqmi_encdec.so \
     vendor/samsung/matisse-common/proprietary/vendor/lib/libqmiservices.so:system/vendor/lib/libqmiservices.so \
     vendor/samsung/matisse-common/proprietary/vendor/lib/libqc-opt.so:system/vendor/lib/libqc-opt.so \
-    vendor/samsung/matisse-common/proprietary/vendor/lib/libqc-iop-client.so:system/vendor/lib/libqti-iop-client.so \
+    vendor/samsung/matisse-common/proprietary/vendor/lib/libqti-iop-client.so:system/vendor/lib/libqti-iop-client.so \
     vendor/samsung/matisse-common/proprietary/lib/libyasalgo.so:system/lib/libyasalgo.so
 
 $(call inherit-product, vendor/qcom/binaries/msm8974/graphics/graphics-vendor.mk)
